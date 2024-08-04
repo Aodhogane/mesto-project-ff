80b98d797c71b2163c62a821d6d9afdc5a6fa309
Илья <152794862+Aodhogane@users.noreply.github.com>
2024-08-05 00:56:15 +0300

    -

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,40 @@
-// @todo: Темплейт карточки
+// Получение шаблона карточки и контейнера для карточек
+const cardTemplate = document.getElementById('card-template').content;
+const cardContainer = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+// Функция создания карточки
+const createCard = (title, imageUrl) => {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция создания карточки
+  cardImage.src = imageUrl;
+  cardImage.alt = title;
+  cardTitle.textContent = title;
 
-// @todo: Функция удаления карточки
+  // Добавление обработчика для кнопки удаления
+  deleteButton.addEventListener('click', () => {
+    cardElement.remove();
+  });
 
-// @todo: Вывести карточки на страницу
+  return cardElement;
+};
+
+// Функция отображения карточек на странице
+const displayCards = (cards) => {
+  cards.forEach(card => {
+    const cardElement = createCard(card.title, card.imageUrl);
+    cardContainer.appendChild(cardElement);
+  });
+};
+
+// Пример использования
+const initialCards = [
+  { title: 'Карточка 1', imageUrl: './images/card1.jpg' },
+  { title: 'Карточка 2', imageUrl: './images/card2.jpg' },
+  { title: 'Карточка 3', imageUrl: './images/card3.jpg'},
+  { title: 'Карточка 3', imageUrl: './images/card3.jpg'}
+];
+
+displayCards(initialCards);

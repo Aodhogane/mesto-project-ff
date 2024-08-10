ac978db81e5f129dcde8702028e0d1fd0db1bb69
Илья <152794862+Aodhogane@users.noreply.github.com>
2024-08-10 22:37:49 +0300

    Добавлениея js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,34 @@
-// @todo: Темплейт карточки
+// Получаем доступ к темплейту карточки
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
 
-// @todo: DOM узлы
+// DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+  
+  // Добавляем обработчик удаления карточки
+  cardDeleteButton.addEventListener('click', deleteCard);
+  
+  return cardElement;
+}
 
-// @todo: Функция удаления карточки
+// Функция удаления карточки
+function deleteCard(event) {
+  const cardElement = event.target.closest('.card');
+  cardElement.remove();
+}
 
-// @todo: Вывести карточки на страницу
+// Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData);
+  placesList.append(cardElement);
+});

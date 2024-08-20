9e1a0d47a7c8bb0eaf8b31f2c20e286f13863632
Илья <152794862+Aodhogane@users.noreply.github.com>
2024-08-21 01:51:36 +0300

    Добаление попоB для картинок.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-    
-   import './pages/index.css'; // Импорт CSS файла
-   import { initialCards } from './scripts/cards.js';
-   
-   // @todo: DOM узлы
-	const placesList = document.querySelector('.places__list');
-	const cardTemplate = document.querySelector('#card-template').content.querySelector(".card");
+import './pages/index.css'; // Импорт CSS файла
+import { initialCards } from './scripts/cards.js';
 
+// @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content.querySelector(".card");
+const popupImage = document.querySelector('.popup_type_image');
+const popupImageContent = popupImage.querySelector('.popup__image');
+const popupImageCaption = popupImage.querySelector('.popup__caption');
+const popupCloseButton = popupImage.querySelector('.popup__close');
 
 // @todo: Функция создания карточки
 function createCard(cardData, deleteCallback) {
@@ -23,12 +25,31 @@ function createCard(cardData, deleteCallback) {
     deleteCallback(cardElement);
   });
 
+  cardImage.addEventListener('click', function () {
+    openImagePopup(cardData.link, cardData.name);
+  });
+
   return cardElement;
 }
 
 // @todo: Функция удаления карточки
 function deleteCard(cardElement) {
     cardElement.remove();
+}
+
+// @todo: Функция открытия попапа с изображением
+function openImagePopup(link, name) {
+  popupImageContent.src = link;
+  popupImageContent.alt = name;
+  popupImageCaption.textContent = name;
+  popupImage.classList.add('popup_is-opened');
+  popupImage.classList.remove('popup_is-animated');
+}
+
+// @todo: Функция закрытия попапа
+function closeImagePopup() {
+  popupImage.classList.add('popup_is-animated');
+  popupImage.classList.remove('popup_is-opened');
 }
 
 // @todo: Вывести карточки на страницу
@@ -40,4 +61,14 @@ function renderCards() {
 }
 
 // Вызов функции для вывода карточек на страницу
-	renderCards();
+renderCards();
+
+// Добавление обработчика события для закрытия попапа
+popupCloseButton.addEventListener('click', closeImagePopup);
+
+// Закрытие попапа при нажатии на пустое пространство вокруг изображения
+popupImage.addEventListener('click', (event) => {
+  if (event.target === popupImage) {
+    closeImagePopup();
+  }
+});

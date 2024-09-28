17b57680716a40cd4f2621ab63b0925590dcf3c7
Илья <152794862+Aodhogane@users.noreply.github.com>
2024-09-28 19:06:04 +0300

    Изменение валидации и modal.

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,15 +1,18 @@
-function openPopup(popup) {
-  popup.classList.add('popup_is-opened');
+export function openPopup(popup) {
+  popup.classList.add('popup_is-animated');
+  setTimeout(() => popup.classList.add('popup_is-opened'), 100);
   document.addEventListener('keydown', handleEscClose);
-}
-function closePopup(popup) {
+  document.addEventListener('click', handleOverlayClose);
+};
+export function closePopup(popup) {
   popup.classList.remove('popup_is-opened');
+  setTimeout(() => popup.classList.remove('popup_is-animated'), 600);
   document.removeEventListener('keydown', handleEscClose);
-}
-function handleEscClose(event) {
-  if (event.key === 'Escape') {
-    const openedPopup = document.querySelector('.popup_is-opened');
-    if (openedPopup) closePopup(openedPopup);
-  }
-}
-export { openPopup, closePopup };
+  document.removeEventListener('click', handleOverlayClose);
+};
+export function handleEscClose(evt) {
+  if (evt.key === 'Escape') closePopup(document.querySelector('.popup_is-opened'));
+};
+export function handleOverlayClose(evt) {
+  if (evt.target.classList.contains('popup')) closePopup(evt.target);
+};
